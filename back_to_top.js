7dd19789da4ba30627bcca28c9d54b41ea696649
Shalini Kashyap <swap.shalini@gmail.com>
2020-01-03 15:19:32 +0530

    Created back to top script file

---
+++ back_to_top.js
@@ -0,0 +1,15 @@
+var $backToTop = $(".back-to-top");
+$backToTop.hide();
+
+$(window).scroll(function() {
+if ($(this).scrollTop()>50) {
+$backToTop.fadeIn();
+} else {
+$backToTop.fadeOut();
+}
+});
+
+$backToTop.find("a").click(function(e) {
+e.preventDefault();
+$("html, body").animate({scrollTop: 0}, 500);
+});
